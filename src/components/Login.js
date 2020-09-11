1856ece7ca2b5d522ea8163ba5089848022e72c5
Techoplite <oriccimirko@gmail.com>
2020-09-11 13:35:19 +0100

    Fix password value props

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,7 @@ const Login = () => {
                 name="password"
                 autoFocus={false}
                 required="true"
-                value={values.email}
+                value={values.password}
                 onChange={handleOnChange}
             />
         </Form>
