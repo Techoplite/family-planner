b4e2e524063c828c3b6bcdf45156b2aa175f8495
Techoplite <oriccimirko@gmail.com>
2020-10-02 10:01:25 +0100

    Add info on anonymous home page

--- src/components/Anonymous.js
+++ src/components/Anonymous.js
@@ -8,8 +8,18 @@ const Anonymous = () => {
 
     return (
         <Switch>
-            This is a work in progress....
-            <Route exact path="/"><div>home page</div></Route>
+            <Route exact path="/">
+                <div>home page</div>
+                <p>This is a work in progress....</p>
+
+                <p>At the moment I have only implemented the authentication system,
+            so feel free to sign up with a dummy email and password.</p>
+
+                <p>You could also join the "Doe family", but I am afraid they have already reached the maximum number of members that can register within a family (9). Anyway if you wan to check for validation their password i "doepass1".</p>
+
+                <p>One last thing. As I develop following a 'mobile-first' approach, if you are viewing it on a deskop screen you might want to righ-click on this webpage, select 'inspect' and then press 'Ctrl + Shift + M'</p>
+
+            </Route>
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
         </Switch>
