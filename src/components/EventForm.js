21f3eab1487c6b8f6db4a0246e1df8bf708ce9f4
Techoplite <oriccimirko@gmail.com>
2020-10-22 10:36:27 +0100

    Fix day conversion

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -86,7 +86,7 @@ const EventForm = (props) => {
     }
 
     const convertDate = (date) => {
-        const day = date.getDate()
+        const day = date.getDate() > 10 ? date.getDate() : "0" + date.getDate()
         const month = date.getMonth() + 1 > 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)
         const year = date.getFullYear()
         return day + "/" + month + "/" + year
