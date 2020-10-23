fe23cdf1cf549fb31117daa99163eb397679724e
Techoplite <oriccimirko@gmail.com>
2020-10-23 10:21:40 +0100

    Fix month display

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -87,7 +87,7 @@ const EventForm = (props) => {
 
     const convertDate = (date) => {
         const day = date.getDate() > 10 ? date.getDate() : "0" + date.getDate()
-        const month = date.getMonth() + 1 > 10 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)
+        const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)
         const year = date.getFullYear()
         return day + "/" + month + "/" + year
     }
