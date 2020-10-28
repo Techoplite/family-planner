48f06c755055ee7fe2dc6e0fd5170fd8c415bd0a
Techoplite <oriccimirko@gmail.com>
2020-10-28 13:02:12 +0000

    Initiate ShoppingList

--- src/components/shopping-list/ShoppingList.js
+++ src/components/shopping-list/ShoppingList.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { makeStyles, Typography } from "@material-ui/core";
 import Paper from '@material-ui/core/Paper';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 
 
 
 const ShoppingList = () => {
+
+    // React
+    const initialState = {
+        shoppingItems: [
+            { text: 'Bleach', shop: 'Tesco', quantity: 3 },
+            { text: 'Butter', shop: '', quantity: 0 },
+            { text: 'Toilet Paper', shop: 'Aldi', quantity: 1 },
+            { text: 'Cheese', shop: 'Asda', quantity: 0 },
+            { text: 'Salad', shop: '', quantity: 0 },
+        ],
+    }
+    const [state, setState] = useState(initialState)
+
+    const handleChange = (e) => {
+        setState({
+            ...state,
+            [e.target.id]: e.target.checked
+        });
+    }
 
     // Material UI
     const useStyles = makeStyles((theme) => ({
@@ -21,14 +42,17 @@ const ShoppingList = () => {
         },
         paper: {
             backgroundColor: '#edd45d',
-            height: '300px',
-            width: '100%',
-            display: 'flex'
+            display: 'flex',
+            alignItems: 'baseline',
+            flexDirection: 'column'
         },
         typography: {
             marginBottom: theme.spacing(4),
-            marginTop: theme.spacing(4)
         },
+        formControlLabel: {
+            marginLeft: theme.spacing(1),
+            marginTop: theme.spacing(1),
+        }
     }));
     const classes = useStyles();
 
@@ -37,10 +61,27 @@ const ShoppingList = () => {
             <ShoppingCartIcon className={classes.icon} />
             <Typography variant="h5" className={classes.typography} >Shopping List</Typography>
             <Paper elevation={3} className={classes.paper}>
-                <FormControlLabel
-                    control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} name="checkedH" />}
-                    label="Custom icon"
-                />
+                {state.shoppingItems.map(item =>
+                    < FormControlLabel
+                        style={{
+                            textDecoration: state[item.text] === true && 'line-through',
+                            color: state[item.text] === true && "#f50057"
+                        }}
+                        key={item.text}
+                        className={classes.formControlLabel}
+                        control={
+                            < Checkbox
+                                id={item.text}
+                                icon={<RadioButtonUncheckedIcon />}
+                                checkedIcon={<CheckCircleOutlineIcon />}
+                                name={item.text}
+                                onChange={handleChange}
+                            />}
+                        label={
+                            `${item.text} ${item.shop && `[${item.shop}]`} 
+                            ${item.quantity !== 0 && `x${item.quantity}`}`}
+                    />
+                )}
             </Paper>
 
         </>
---
+++ src/components/todo-list/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+
+const TodoList = () => {
+    return (
+        <div>This page will host the to do list app...</div>
+    );
+}
+
+export default withRouter(TodoList);
