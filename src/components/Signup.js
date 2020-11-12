f108efba980bc51a8ee99f598386684ec7ed3243
Techoplite <oriccimirko@gmail.com>
2020-11-12 09:08:07 +0000

    Add margin bottom

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -254,7 +254,7 @@ const Signup = (props) => {
 
     const classes = useStyles()
     return (
-        <div style={{ padding: "0 16px", marginTop: "144px" }}>
+        <div style={{ padding: "0 16px", marginTop: "144px", marginBottom: "2rem" }}>
             {auth.email && <Redirect to='/' />}
             <AccountCircleIcon className={classes.icon} />
             <Form title="Sign up">
